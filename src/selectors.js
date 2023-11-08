8796bedf41af12068449ded1e52126bef71c5071
aungzinlat <aungzinlat986@gmail.com>
2023-11-08 20:45:53 +0630

    error

--- src/selectors.js
+++ src/selectors.js
@@ -1,10 +1,9 @@
 export const searchInput = document.querySelector("#searchInput");
 export const btn = document.querySelector("#btn");
-export const weatherIcon = document.querySelector("#weather-icon");
+export const weatherIcon = document.querySelector("#weatherIcon");
 export const humidity = document.querySelector("#humidity");
 export const wind = document.querySelector("#wind");
 export const cityName = document.querySelector("#city");
 export const temp = document.querySelector("#temp");
 export const weather = document.querySelector("#weather");
 export const error = document.querySelector("#error");
-
